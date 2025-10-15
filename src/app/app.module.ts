fd41bcea7714f96d6314269e13f9b2175bac1d94
Mac <mac@MacBook-Pro-Mac.local>
2025-10-15 20:59:17 +0300

    Added a title to the create page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MAT_DATE_FORMATS } from '@angular/material/core';
+import { MY_DATE_FORMATS } from './app.types';
 
 @NgModule({
   declarations: [],
   imports: [BrowserModule, AppComponent],
-  providers: [],
+  providers: [{ provide: MAT_DATE_FORMATS, useValue: MY_DATE_FORMATS }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/app.types.ts
+++ src/app/app.types.ts
@@ -3,3 +3,13 @@ export interface Reminder {
   birthday: string;
   photoUrl: string | null;
 }
+
+export const MY_DATE_FORMATS = {
+  parse: { dateInput: 'DD/MM/YYYY' },
+  display: {
+    dateInput: 'DD/MM/YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
